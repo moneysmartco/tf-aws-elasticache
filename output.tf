b5dfb665bb886682527acb7f0986e2c50fb69b28
Eric Ho <dho.eric@gmail.com>
2019-09-12 17:28:55 +0800

    Add tags

--- output.tf
+++ output.tf
@@ -3,12 +3,12 @@ output "elasticache_sg_id" {
 }
 
 # Condition doesn't work here for condition ? 1 : 0
-# Check later on the newer version 
+# Check later on the newer version
 #  value = "${elasticache_number_cache_clusters >= 2 ? aws_elasticache_replication_group.cerberus_redis.primary_endpoint_address : aws_elasticache_cluster.elasticache.cache_nodes.0.address}"
 output "elasticache_replica_url" {
-  value = "${aws_elasticache_replication_group.cerberus_redis.primary_endpoint_address}"
+  value = "${var.elasticache_number_cache_clusters >= 2 ? join( ", ", aws_elasticache_replication_group.cerberus_redis.*.primary_endpoint_address) : ""}"
 }
 
 output "elasticache_url" {
-  value = "${aws_elasticache_cluster.elasticache.cache_nodes.0.address}"
+  value = "${var.elasticache_number_cache_clusters == 1 ? aws_elasticache_cluster.elasticache.cache_nodes.0.address : ""}"
 }
