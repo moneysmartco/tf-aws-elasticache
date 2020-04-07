607289fc71970fd31cb3b5b276b556510f6dc47c
eugeneho-moneysmart <eugene.ho@moneysmart.com>
2020-04-07 17:42:51 +0800

    for skipping output

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
-output "elasticache_sg_id" {
-  value = "${aws_security_group.elasticache_sg.id}"
-}
+# output "elasticache_sg_id" {
+#   value = "${aws_security_group.elasticache_sg.id}"
+# }
 
-# Condition doesn't work here for condition ? 1 : 0
-# Check later on the newer version
-#  value = "${elasticache_number_cache_clusters >= 2 ? aws_elasticache_replication_group.cerberus_redis.primary_endpoint_address : aws_elasticache_cluster.elasticache.cache_nodes.0.address}"
-output "elasticache_replica_url" {
-  value = "${var.elasticache_number_cache_clusters >= 2 ? join( ", ", aws_elasticache_replication_group.cerberus_redis.*.primary_endpoint_address) : ""}"
-}
+# # Condition doesn't work here for condition ? 1 : 0
+# # Check later on the newer version
+# #  value = "${elasticache_number_cache_clusters >= 2 ? aws_elasticache_replication_group.cerberus_redis.primary_endpoint_address : aws_elasticache_cluster.elasticache.cache_nodes.0.address}"
+# output "elasticache_replica_url" {
+#   value = "${var.elasticache_number_cache_clusters >= 2 ? join( ", ", aws_elasticache_replication_group.cerberus_redis.*.primary_endpoint_address) : ""}"
+# }
 
-output "elasticache_url" {
-  value = "${var.elasticache_number_cache_clusters == 1 ? join( ", ", aws_elasticache_cluster.elasticache.*.cache_nodes.0.address) : ""}"
-}
+# output "elasticache_url" {
+#   value = "${var.elasticache_number_cache_clusters == 1 ? join( ", ", aws_elasticache_cluster.elasticache.*.cache_nodes.0.address) : ""}"
+# }
