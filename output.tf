b9447647149b73e8ab2c595ac25ad151a8a88e8a
apoorvshrivastava-ms <apoorv.shrivastava@moneysmart.com>
2021-01-20 15:46:45 +0530

    count set

--- output.tf
+++ output.tf
@@ -6,7 +6,7 @@ output "elasticache_sg_id" {
 # Check later on the newer version
 
 output "elasticache_url" {
-  value = "${var.elasticache_number_cache_clusters == 1 ? aws_elasticache_cluster.elasticache.cache_nodes.0.address : ""}"
+  value = "${var.elasticache_number_cache_clusters == 1 ? aws_elasticache_cluster.elasticache.1.cache_nodes.0.address : ""}"
 }
 
 # output "elasticache_url" {
