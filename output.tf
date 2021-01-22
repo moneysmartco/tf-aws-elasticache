0184611024bf5d60457aae7a1134d83ddca5dc2d
apoorvshrivastava-ms <apoorv.shrivastava@moneysmart.com>
2021-01-22 15:05:00 +0530

    final commit

--- output.tf
+++ output.tf
@@ -2,22 +2,12 @@ output "elasticache_sg_id" {
   value = aws_security_group.elasticache_sg.id
 }
 
-# Condition doesn't work here for condition ? 1 : 0
-# Check later on the newer version
 
 output "elasticache_url" {
   # value = var.single_node == "true" ? aws_elasticache_cluster.elasticache.*.cache_nodes.0.address :["random"]
   value = var.single_node == true ? aws_elasticache_cluster.elasticache.*.cache_nodes.0.address : [""]
 
 }
-
-# output "elasticache_url" {
-#   value = aws_elasticache_cluster.elasticache.0.cache_nodes.0.address
-# }
-
-#output "elasticache_url" {
-#   value = "${elasticache_number_cache_clusters >= 2 ? aws_elasticache_replication_group.cerberus_redis.primary_endpoint_address : aws_elasticache_cluster.elasticache.cache_nodes.0.address}"
-#}
 
 
 output "elasticache_replica_url" {
@@ -27,7 +17,4 @@ output "elasticache_replica_url" {
   )
 }
 
-#output "elasticache_url" {
-#value = "${var.elasticache_number_cache_clusters == 1 ? join( ", ", aws_elasticache_cluster.elasticache.cache_nodes.*.address) : ""}"
-#  value = "${var.elasticache_number_cache_clusters == 1 ? join( ", ", aws_elasticache_cluster.elasticache.*.cache_nodes.0.address) : ""}"
-#}
+
