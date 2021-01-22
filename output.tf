0960bc4c5eeb725a790188918e75ff756f4a02d1
apoorvshrivastava-ms <apoorv.shrivastava@moneysmart.com>
2021-01-22 10:29:30 +0530

    reverting output order

--- output.tf
+++ output.tf
@@ -6,7 +6,7 @@ output "elasticache_sg_id" {
 # Check later on the newer version
 
 output "elasticache_url" {
-  value = var.single_node == "true" ? ["random"] : aws_elasticache_cluster.elasticache.*.cache_nodes.0.address
+  value = var.single_node == "true" ? aws_elasticache_cluster.elasticache.*.cache_nodes.0.address :["random"]
 }
 
 # output "elasticache_url" {
